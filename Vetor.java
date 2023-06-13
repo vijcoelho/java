13fe40988df31e288bbbc41521172006c2609873
Vitor Coelho <vijcoelho@gmail.com>
2023-06-12 22:57:11 -0300

    Create Vetor.java

---
+++ Vetor.java
@@ -0,0 +1,13 @@
+public class Vetor { 
+  public static void main(String[] args) { 
+    String [] cars = {"Camaro", "Cruze", "Ferrari", "Porche"};
+    // for (String i : cars) {
+    // System.out.println(cars.length);  = len() em Python
+    // for (int i = 0; i < cars.length; i++) {
+    // System.out.println(cars[i]); Faz a mesma coisa do de baixo kk
+    //     }
+    // for (String i : cars) {      Faz a mesma coisa do de cima
+    //     System.out.println(i);
+    // }
+    }
+}
