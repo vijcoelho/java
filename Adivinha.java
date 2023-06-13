c7e03c24353467a04271cda06147d0cdef26afb6
Vitor Coelho <vijcoelho@gmail.com>
2023-06-13 12:41:41 -0300

    up

---
+++ Adivinha.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Adivinha {
+    public static void jogo() {
+        System.out.println("Bem vindo ao Adivinha em JAVA.");
+        Scanner scanner = new Scanner(System.in);
+        S
+        int num = scanner.nextInt();
+    }
+}
--- Vetor.java
+++ Vetor.java
@@ -5,9 +5,9 @@ public class Vetor {
     // System.out.println(cars.length);  = len() em Python
     // for (int i = 0; i < cars.length; i++) {
     // System.out.println(cars[i]); Faz a mesma coisa do de baixo kk
-    //     }
-    // for (String i : cars) {      Faz a mesma coisa do de cima
-    //     System.out.println(i);
-    // }
+    for (String i : cars) {      
+       System.out.println(i);
+   }
+  //  Faz a mesma coisa do de cima
     }
 }
