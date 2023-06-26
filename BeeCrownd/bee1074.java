a60f7572b5f3cddbdaadcbbcc7c0b32d5e96b84c
Vitor Coelho <vijcoelho@gmail.com>
2023-06-26 17:47:28 -0300

    upp

---
+++ BeeCrownd/bee1074.java
@@ -0,0 +1,22 @@
+package BeeCrownd;
+
+import java.util.Scanner;
+
+public class bee1074 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a;
+        for (int i = 0; i < 5; i++) {
+            a = scanner.nextInt();
+
+            if (a == 0) {
+                System.out.println("NULL");
+            } if (a % 2 == 0 && a > 0) {
+                 System.out.println("EVEN POSITIVE");
+            } if (a % 2 != 0 && a < 0) {
+                System.out.println("ODD NEGATIVE");
+            }
+        }
+        scanner.close();
+    }
+}
---
+++ BeeCrownd/bee1080.java
@@ -0,0 +1,30 @@
+package BeeCrownd;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class bee1080 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> vetor = new ArrayList<Integer>();
+
+        int number = 0;
+        int position = 0;
+
+        for (int i = 1; i < 101; i++) {
+            System.out.print("Numero posicao " + i + " : ");
+            number = scanner.nextInt();
+            vetor.add(number);
+            if (number > vetor.get(position)) {
+                position = i - 1;
+            }
+        }
+
+        int bigNumber = vetor.get(position);
+
+        System.out.println("O maior numero é " + bigNumber);
+        System.out.println("A posição do maior numero é " + (position + 1));
+
+        scanner.close();
+    }
+}
