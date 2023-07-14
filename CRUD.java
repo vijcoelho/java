a9e32fc096531f7438c2792fae22d2f51f1da9f6
Vitor Coelho <vijcoelho@gmail.com>
2023-07-14 18:07:01 -0300

    Update CRUD.java

--- CRUD.java
+++ CRUD.java
@@ -1,46 +1,82 @@
 import java.util.Scanner;
 
 public class CRUD {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        String nome = "";
-        int idade = 0;
-        int codigo = 0;
-        int opcao = 0;
     
-        while (true) {
-            
-            System.out.println("Escolha a opcao abaixo:");
-            System.out.println("0 - Sair");
-            System.out.println("1 - Inserir dados");
-            System.out.println("2 - Alterar dados");
-            System.out.println("3 - Pesquisar dados");
-            System.out.println("4 - Deletar dados");
+    private static Scanner scanner = new Scanner(System.in);
 
-            opcao = scanner.nextInt();
+    static void menu() {
             
-            switch (opcao) {
-                case 1: 
-                System.out.print("Insira o nome: ");
-                nome = scanner.next();
-                System.out.print("Insira a idade: ");
-                idade =  scanner.nextInt();
-                System.out.print("Insira o codigo: ");
-                codigo = scanner.nextInt();
+        System.out.println("Escolha a opcao abaixo:");
+        System.out.println("0 - Sair");
+        System.out.println("1 - Inserir dados");
+        System.out.println("2 - Alterar dados");
+        System.out.println("3 - Pesquisar dados");
+        System.out.println("4 - Deletar dados");
 
-                case 2:
-                System.out.print("Insira o novo nome: ");
-                String novo_nome = scanner.next();
-                nome = novo_nome;
-                System.out.printf("Novo nome: %s",nome);
-                System.out.print("Insira a nova idade: ");
-                int nova_idade =  scanner.nextInt();
+    }
+    
+    static void opcao1() {
+        
+        System.out.print("\nInsira o nome: ");
+        String nome = scanner.next();
+        System.out.println("Seu nome é: "+ nome);
+        
+        System.out.print("\n Insira a idade: ");
+        int idade =  scanner.nextInt();
+        System.out.println("Sua idade é: "+ idade);
+        
+        System.out.print("\nInsira o codigo: ");
+        int codigo = scanner.nextInt();
+        System.out.println("Seu codigo é: "+ codigo);
+        
+        scanner.close();
+    }
+
+    static void opcao2 (String nome, int idade) {
+    
+        System.out.print("Seu nome atual é " + nome +" . Deseja mudar? [s/n]: ");
+        String pergunta = scanner.next();
+        if (pergunta == "s" && pergunta == "S") {
+            
+            System.out.print("Insira seu novo nome: ");
+            String novo_nome = scanner.next();
+            nome = novo_nome;
+        }
+        else if (pergunta == "n" && pergunta == "N") {
+            
+            System.out.printf("Entao seu nome continuara o mesmo. (%s)",nome);
+
+            System.out.println("Deseja entao mudar a sua idade. [s/n]");
+            String pergunta_2 = scanner.next();
+            
+            if (pergunta_2 == "s" && pergunta_2 == "S") {
+                
+                System.out.print("Insira sua nova idade: ");
+                int nova_idade = scanner.nextInt();
                 idade = nova_idade;
-                System.out.printf("Nova idade: %s",idade);
-
+            }
+            else {
+                System.out.printf("Ok, sua idade continuara como estava (%s)",idade);
             }
         }
-        
+        else {
+            System.out.println("Apenas [s/n].");
+        }
+    }
+    
+    public static void main(String[] args) {
+        while (true) {
+            menu();
+            int opcao = scanner.nextInt();
+            if (opcao == 0) {
+                break;
+            }
+            else if (opcao == 1) {
+                opcao1();
+            }
+            else if (opcao == 2) {
+                opcao2("unknow", 0);
+            }
+        }        
     }
 }
