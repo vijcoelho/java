daa870f96d1360c6d0f33c9664b3a75fdabbe13c
Vitor Coelho <vijcoelho@gmail.com>
2023-08-03 14:28:54 -0300

    Create Testes.java

---
+++ Testes.java
@@ -0,0 +1,9 @@
+public class Testes {
+    public static void main(String [] args) {
+        int [] vetor = {2,4,6,8,10,12,14,16,18,20};
+
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.println(vetor[i]);
+        }
+    }
+}
