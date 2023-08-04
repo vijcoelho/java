a875d59917be4e6f6ffa141d7979f17ead437f50
Vitor Coelho <vijcoelho@gmail.com>
2023-08-04 16:03:18 -0300

    Update Testes.java

--- Testes.java
+++ Testes.java
@@ -1,9 +1,7 @@
 public class Testes {
-    public static void main(String [] args) {
-        int [] vetor = {2,4,6,8,10,12,14,16,18,20};
-
-        for (int i = 0; i < vetor.length; i++) {
-            System.out.println(vetor[i]);
+    public static void main(String[] args) {
+        for (int i = 1; i < 11; i++) {
+            System.out.println(i);
         }
     }
 }
