5492c68ef277ab7b7f4b79338469883b1d270313
Vitor Coelho <vijcoelho@gmail.com>
2023-09-04 18:57:28 -0300

    up

--- Lista_Classe_CTC/classe_calculadora.java
+++ Lista_Classe_CTC/classe_calculadora.java
@@ -3,47 +3,78 @@ package Lista_Classe_CTC;
 import java.util.Scanner;
 
 class Calculadora {
+    
+    private int num;
+    private int num2;
+    private int pergunta;
+    private Scanner scanner;
 
-    public void Operacoes(int num, int num2) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("\u001b[2J\u001b[H");
-        System.out.print("Escolha sua operacao (+,-,/ ou x): ");
-        String pergunta = scanner.next();
+    public Calculadora() {
+        scanner = new Scanner(System.in);
+    }
+
+    public void chooseNumber() {
+        System.out.println("\u001b[2J\u001b[H");
+        System.out.print("PRIMEIRO NUMERO: ");
+        num = scanner.nextInt();
+        System.out.print("SEGUNDO NUMERO: ");
+        num2 = scanner.nextInt();
+    }
+
+    public void soma() {
+        System.out.println(num + num2);
+    } 
+
+    public void subtracao() {
+        System.out.println(num - num2);
+    }
+
+    public void multi() {
+        System.out.println(num * num2);
+    }
+
+    public void div() {
+        System.out.println(num / num2);
+    }
+
+    public void contas() {
+        System.out.println("\u001b[2J\u001b[H");
+        System.out.println("1- SOMA | 2- SUB | 3- MULTI | 4- DIVISAO");
+        System.out.print("ESCOLHA A CONTA DESEJADA: ");
+        pergunta = scanner.nextInt();
         switch (pergunta) {
-            case "+":
-                int soma = num + num2;
-                System.out.println(num+" + "+ num2 +" = "+ soma);
+            case 1:
+                System.out.println("\u001b[2J\u001b[H");
+                System.out.print("SOMA: " );
+                soma();
                 break;
-            case "-":
-                int sub = num - num2;
-                System.out.println(num+" - "+ num2 +" = "+sub);
+
+            case 2:
+                System.out.println("\u001b[2J\u001b[H");
+                System.out.print("SUBTRACAO: ");
+                subtracao();
                 break;
-            case "/":
-                int div = num / num2;
-                System.out.println(num+" / "+ num2 +" = "+ div);
+
+            case 3:
+                System.out.println("\u001b[2J\u001b[H");
+                System.out.print("MULTIPLICACAO: ");
+                multi();
                 break;
-            case "x":
-                int mult = num * num2;
-                System.out.println(num+" x "+ num2 +" = "+ mult);
+
+            case 4:
+                System.out.println("\u001b[2J\u001b[H");
+                System.out.print("DIVISAO: ");
+                div();
                 break;
+
             default:
-                System.out.println("ERROR");
-                break;
+                System.out.println("Escolha inválida!");
         }
-        scanner.close();
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         Calculadora calculadora = new Calculadora();
-        
-        System.out.print("\u001b[2J\u001b[H");
-        System.out.print("Valor do primeiro número: ");
-        int num = scanner.nextInt();
-        System.out.print("Valor do segundo número: ");
-        int num2 = scanner.nextInt();
-        
-        calculadora.Operacoes(num, num2);
-        scanner.close();
+        calculadora.chooseNumber();
+        calculadora.contas();
     }
 }
