92849c1e4c7fb7635d1bb52431a62a1f193d5962
Vitor Coelho <vijcoelho@gmail.com>
2023-09-05 18:40:26 -0300

    Create classeNumeros.java

---
+++ Exc_Classes/classeNumeros.java
@@ -0,0 +1,13 @@
+package Exc_Classes;
+
+class Numeros {
+    public int[] vetor;
+
+    public Numeros(int[] _valores) {
+        vetor = _valores;
+    }
+
+    public void localizaMaior() {
+        
+    }
+}
