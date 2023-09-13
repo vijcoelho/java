e038042bd1094d5658d8b2918b194ecda24d2050
Vitor Coelho <vijcoelho@gmail.com>
2023-09-13 14:46:43 -0300

    Update loja.java

--- BD/loja.java
+++ BD/loja.java
@@ -7,12 +7,12 @@ import java.sql.SQLException;
 public class loja {
     public Connection getConnection() {
         try {
+
             return DriverManager.getConnection(
             "jdbc:sqlserver://DESKTOP-BCKMJI3\\SQLEXPRESS;databaseName=loja;user=vitor;password=teste123"
             );
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-        
     }
 }
