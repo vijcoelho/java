0a7a865b10f17ee68edaec9dcf62ae96d4e70095
Vitor Coelho <130684606+vitorjosecoelho@users.noreply.github.com>
2023-09-14 20:54:29 -0300

    up

--- BD/loja.java
+++ BD/loja.java
@@ -7,12 +7,15 @@ import java.sql.SQLException;
 public class Loja {
     public Connection getConnection() {
         try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             return DriverManager.getConnection(
             "jdbc:sqlserver://DESKTOP-BCKMJI3\\SQLEXPRESS;databaseName=loja;user=vitor;password=teste123"
             );
         } catch (SQLException e) {
             throw new RuntimeException(e);
-        }
+        } catch (ClassNotFoundException e){
+            throw new RuntimeException(e);
+        } 
         
     }
 }
