a361aa8de4734fbeda5bbba76a3e4c0adfc6a99e
Vitor Coelho <vijcoelho@gmail.com>
2023-09-15 16:01:35 -0300

    Update loja.java

--- BD/loja.java
+++ BD/loja.java
@@ -6,6 +6,7 @@ import java.sql.SQLException;
 
 public class Loja {
     public Connection getConnection() {
+        
         try {
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             return DriverManager.getConnection(
