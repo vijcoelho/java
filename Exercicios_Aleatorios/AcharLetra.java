858271a5152f96073229f64ef0db36ed9f30ce71
Vitor Coelho <130684606+vitorjosecoelho@users.noreply.github.com>
2023-09-25 21:42:44 -0300

    up

---
+++ Exercicios_Aleatorios/AcharLetra.java
@@ -0,0 +1,9 @@
+package Exercicios_Aleatorios;
+
+import java.util.Scanner;
+
+public class AcharLetra {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
