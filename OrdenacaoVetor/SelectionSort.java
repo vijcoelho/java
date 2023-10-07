691c98e8c664ab42456f794b27aa8e8ad8e34f39
Vitor Coelho <130684606+vitorjosecoelho@users.noreply.github.com>
2023-10-06 22:57:59 -0300

    up

--- OrdenacaoVetor/SelectionSort.java
+++ OrdenacaoVetor/SelectionSort.java
@@ -1,23 +1,20 @@
 package OrdenacaoVetor;
 
-public class BubbleSort {
+public class SelectionSort {
     public static void main(String[] args) {
         int[] vetor = {3,5,1,2,4};
-
+        
         System.out.print("Vetor desorganizado: ");
         for (int i = 0; i < vetor.length; i++) {
             System.out.print(vetor[i] + " ");
         }
 
-        for(int i = 0; i < vetor.length; i++) {
-            for (int j = 0; j < i; j++) {
-                if(vetor[j] > vetor[j+1]) {
-                    int temp = vetor[j];
-                    vetor[j] = vetor[j+1];
-                    vetor[j+1] = temp;
-                }
-            }
-        }
+        
+
+
+
+
+        
 
         System.out.println("");
         System.out.print("Vetor organizado: ");
