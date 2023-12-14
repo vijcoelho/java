be9f50156ffd7680cd493cbd3f2ac8c032a47a86
Vitor Coelho <130684606+vitorjosecoelho@users.noreply.github.com>
2023-12-14 00:47:56 -0300

    Update Testes.java

--- Testes.java
+++ Testes.java
@@ -1,7 +1,7 @@
 public class Testes {
     public static void main(String[] args) {
         
-        int vetor[] = {1,2,3,4,5,6,77,8,9};
+        int vetor[] = {9,5,2,3,1,4,7,6,10};
         int maior = vetor[0];
 
         for (int i = 0; i < vetor.length; i++) {
@@ -9,7 +9,20 @@ public class Testes {
                 maior = vetor[i];
             }
         }
+        System.out.println("Maior: "+ maior);
 
-        System.out.println(maior);
+        for(int i = 0; i < vetor.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if(vetor[j] > vetor[j + 1]) {
+                    int aux = vetor[j];
+                    vetor[j] = vetor[j + 1];
+                    vetor[j + 1] = aux;
+                }
+            }
+        }
+
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.print(vetor[i] + " ");
+        }
     }
 }
